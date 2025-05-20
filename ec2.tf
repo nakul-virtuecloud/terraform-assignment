1ec180aa6d41e7e938d572d8510982d5490fd770
nakul-virtuecloud <your-new-email@virtuecloud.com>
2025-05-20 13:52:13 +0530

    updated code

--- ec2.tf
+++ ec2.tf
@@ -1,30 +1,14 @@
 # Create a new instance of the latest Ubuntu 14.04 on an
 # t2.micro node with an AWS Tag naming it "HelloWorld"
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-south-1"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+resource "aws_instance" "my_ec2_instance" {
+  ami           = "ami-00bb6a80f01f03502"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "firstinstance"
   }
 }
